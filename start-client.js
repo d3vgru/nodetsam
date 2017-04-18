5a5d9ebb077c99ba52d8cdb106e1fbc05aa89dcc
Ed Knutson <knutsoned@gmail.com>
2017-04-17 19:19:44 -0500

    adding LoopBack and reorganizing server code

--- start-client.js
+++ start-client.js
@@ -1,3 +1,3 @@
 const args = [ 'start' ];
-const opts = { stdio: 'inherit', cwd: 'client', shell: true };
+const opts = { stdio: 'inherit', cwd: 'client/reference', shell: true };
 require('child_process').spawn('npm', args, opts);

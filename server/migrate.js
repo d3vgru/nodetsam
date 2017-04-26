a6d3eaf15991a7d2dce31227938cf3862d1ace68
Ed Knutson <knutsoned@gmail.com>
2017-04-26 18:16:23 -0500

    add migration script

---
+++ server/migrate.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var loopback = require('loopback');
+var boot = require('loopback-boot');
+
+var app = module.exports = loopback();
+
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(app, __dirname, function(err) {
+  if (err) throw err;
+});
+
+app.dataSources.mysql.automigrate();
+console.log("Performed automigration.");
+process.exit();
